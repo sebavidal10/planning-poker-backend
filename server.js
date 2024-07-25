29408c4f5a427479ed99d0542cedfe36f52d4639
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2024-07-24 22:32:04 -0400

    add delete votes and display users online

--- server.js
+++ server.js
@@ -9,11 +9,10 @@ require('dotenv').config();
 const app = express();
 const server = http.createServer(app);
 
-// Configura CORS para Express
 app.use(
   cors({
-    origin: process.env.FRONT_URL, // Cambia esto si tu frontend está en otra URL
-    methods: ['GET', 'POST'],
+    origin: process.env.FRONT_URL,
+    methods: ['GET', 'POST', 'DELETE'],
     allowedHeaders: ['Content-Type'],
   })
 );
@@ -21,58 +20,109 @@ app.use(express.json());
 
 const io = socketIo(server, {
   cors: {
-    origin: process.env.FRONT_URL, // Cambia esto si tu frontend está en otra URL
+    origin: process.env.FRONT_URL,
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type'],
   },
 });
 
-// Obtener resultados
-app.get('/results/:votingInstanceName', async (req, res) => {
-  console.log('Obteniendo resultados...');
-  const { votingInstanceName } = req.params;
-  const results = await UserVote.find({ votingInstanceName });
-  console.log('Resultados:', results);
+let participants = {};
+let socketIdToUserMap = {};
 
-  res.json(results);
+app.get('/results/:votingInstanceName', async (req, res) => {
+  try {
+    const { votingInstanceName } = req.params;
+    const results = await UserVote.find({ votingInstanceName });
+    res.json(results);
+  } catch (error) {
+    console.error('Error fetching results:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
-// Manejar conexión de socket.io
+app.delete('/results/:votingInstanceName', async (req, res) => {
+  try {
+    const { votingInstanceName } = req.params;
+    await UserVote.deleteMany({ votingInstanceName });
+    participants[votingInstanceName] = participants[votingInstanceName].map(
+      (participant) => ({
+        ...participant,
+        hasVoted: false,
+      })
+    );
+    io.emit('updateParticipants', {
+      votingInstanceName,
+      participants: participants[votingInstanceName],
+    });
+    res.status(200).send('Votes deleted');
+  } catch (error) {
+    console.error('Error deleting votes:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado:', socket.id);
 
-  // Manejar selección de voto
-  socket.on('selectVote', ({ name, vote, votingInstanceName }) => {
-    console.log(`${name} seleccionó ${vote}`);
+  socket.on('join', async ({ name, votingInstanceName }) => {
     try {
-      UserVote.updateOne(
-        {
-          name,
-          votingInstanceName,
-        },
-        {
-          name,
-          vote,
-          votingInstanceName,
-          date: new Date(),
-        },
-        { upsert: true }
-      ).then(() => {
-        console.log('Voto guardado', { name, vote, votingInstanceName });
+      if (!participants[votingInstanceName]) {
+        participants[votingInstanceName] = [];
+      }
+      const existingVote = await UserVote.findOne({ name, votingInstanceName });
+      participants[votingInstanceName].push({ name, hasVoted: !!existingVote });
+      socketIdToUserMap[socket.id] = { name, votingInstanceName };
+      io.emit('updateParticipants', {
+        votingInstanceName,
+        participants: participants[votingInstanceName],
       });
+    } catch (error) {
+      console.error('Error during join:', error);
+    }
+  });
+
+  socket.on('selectVote', async ({ name, vote, votingInstanceName }) => {
+    try {
+      await UserVote.updateOne(
+        { name, votingInstanceName },
+        { name, vote, votingInstanceName, hasVoted: true, date: new Date() },
+        { upsert: true }
+      );
+      const participant = participants[votingInstanceName].find(
+        (p) => p.name === name
+      );
+      if (participant) {
+        participant.hasVoted = true;
+      }
+      io.emit('updateParticipants', {
+        votingInstanceName,
+        participants: participants[votingInstanceName],
+      });
+      console.log('Voto guardado', { name, vote, votingInstanceName });
     } catch (error) {
       console.error('Error saving vote:', error);
     }
   });
 
-  // Manejar inicio de temporizador
   socket.on('startTimer', () => {
-    console.log('Temporizador iniciado...');
-    io.emit('timerTick'); // Ejemplo de evento de tick del temporizador
+    io.emit('timerTick');
   });
 
-  // Manejar desconexión
   socket.on('disconnect', () => {
+    const user = socketIdToUserMap[socket.id];
+    if (user) {
+      const { name, votingInstanceName } = user;
+      if (participants[votingInstanceName]) {
+        participants[votingInstanceName] = participants[
+          votingInstanceName
+        ].filter((participant) => participant.name !== name);
+        io.emit('updateParticipants', {
+          votingInstanceName,
+          participants: participants[votingInstanceName],
+        });
+      }
+      delete socketIdToUserMap[socket.id];
+    }
     console.log('Cliente desconectado:', socket.id);
   });
 });
